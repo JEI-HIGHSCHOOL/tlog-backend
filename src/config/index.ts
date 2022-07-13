6687f28aa134c9b48445b1fba57214d8a96b0bee
kiss8981 <kdh89811@gmail.com>
2022-07-13 16:39:40 +0900

    feat: image

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, DAUM_EMAIL, DAUM_PASSWORD } = process.env;
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, DAUM_EMAIL, DAUM_PASSWORD, ACCESS_KEY_ID, SECREST_ACCESS_KEY, REGION } = process.env;
--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,16 +1,29 @@
 import { HttpException } from '@/exceptions/HttpException';
 import { RequestWithUser } from '@/interfaces/auth.interface';
+import { S3 } from '@aws-sdk/client-s3';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { ACCESS_KEY_ID, SECREST_ACCESS_KEY, REGION } from "@/config"
 
-const ImageStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images/');
+const s3 = new S3({
+  credentials: {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECREST_ACCESS_KEY
   },
-
-  filename: (req: RequestWithUser, file, cb) => {
-    cb(null, `${req.user.id}_${Date.now()}.jpg`);
+  region: REGION
+})
+const ImageStorage = multerS3({
+  s3: s3,
+  bucket: "tlogcdn",
+  acl: 'public-read',
+  metadata: function(req, file, cb) {
+    cb(null, {
+      fieldName: file.fieldname
+    })
   },
-});
+  key: function(req: RequestWithUser, file, cb) {
+    cb(null, `${req.user.id}_${Date.now()}.jpg`);  }
+})
 
 export const imageUpload = multer({
   storage: ImageStorage,
